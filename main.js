7cfc79318050fd67cd99f7d892ad340bb1529759
iceman730806 <iceman730806@gmail.com>
2020-10-12 16:54:55 +0800

    phone

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 let express = require('express');
 let app = express();
 let port = process.env.PORT || 8888
-let user_router = require('./routers/main_router.js')
+let phone_router = require('./routers/main_router.js')
 
 app.get("/", (req, res) => {
     res.send("This is main page.");
 })
 
-app.use("/user", user_router)
+app.use("/phone", phone_router)
 
 app.listen(port, () => {
     console.log(`server listen on port ${port}`)
